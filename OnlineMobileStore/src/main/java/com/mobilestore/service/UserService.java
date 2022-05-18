f20c3268844deded3e50c5e01d9154e9980b8d15
nikailas <nikailas@INLEN8520052025.corp.capgemini.com>
2022-05-18 18:41:58 +0530

    Pushing files

---
+++ OnlineMobileStore/src/main/java/com/mobilestore/service/UserService.java
@@ -0,0 +1,16 @@
+package com.mobilestore.service;
+	import java.util.Collection;
+
+import com.mobilestore.entity.User;
+
+	
+	public interface UserService {
+	    User findOne(String email);
+
+	    Collection<User> findByRole(String role);
+
+	    User save(User user);
+
+	    User update(User user);
+	}
+
