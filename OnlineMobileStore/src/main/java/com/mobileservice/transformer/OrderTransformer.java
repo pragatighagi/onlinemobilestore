572d7ab1b567c9868079cad60609cbd52175e3b5
nikailas <nikailas@INLEN8520052025.corp.capgemini.com>
2022-05-23 15:03:40 +0530

    hi

---
+++ OnlineMobileStore/src/main/java/com/mobileservice/transformer/OrderTransformer.java
@@ -0,0 +1,45 @@
+package com.mobileservice.transformer;
+import com.mobilestore.entity.OrderEntity;
+	import com.mobilestore.model.Order;
+
+	public class OrderTransformer{
+			public static com.mobilestore.entity.OrderEntity transformOrder(com.mobilestore.entity.OrderEntity order)
+			{
+				if (order != null) {
+					OrderEntity entity = new com.mobilestore.entity.OrderEntity();
+					entity.setOrderDate(order.getOrderDate());
+					entity.setDispatchDate(order.getDispatchDate());
+					entity.setQuantity(order.getQuantity());
+					entity.setCost(order.getCost());
+					entity.setTotalcost(order.getTotalcost());
+					entity.setStatus(order.getStatus());
+					entity.setCustomer(order.getCustomer());
+					
+					
+					return entity;
+				}
+				return null;
+			}
+
+			public static OrderEntity transformOrderEntity(com.mobilestore.entity.OrderEntity entity) {
+				if (entity != null) {
+					com.mobilestore.entity.OrderEntity newOrd = new com.mobilestore.entity.OrderEntity();
+					newOrd.setOrderDate(entity.getOrderDate());
+					newOrd.setDispatchDate(entity.getDispatchDate());
+					newOrd.setQuantity(entity.getQuantity());
+					newOrd.setCost(entity.getCost());
+					newOrd.setTotalcost(entity.getTotalcost());
+					newOrd.setStatus(entity.getStatus());
+					newOrd.setCustomer(entity.getCustomer());
+					
+					
+					return newOrd;
+				}
+				return null;
+			}
+			public static OrderEntity transformCustomer(Order order) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		}
+
---
+++ OnlineMobileStore/src/main/java/com/mobileservice/transformer/UserTransformer.java
@@ -0,0 +1,36 @@
+package com.mobileservice.transformer;
+import com.mobilestore.entity.UserEntity;
+import com.mobilestore.model.User;
+
+	public class UserTransformer {
+
+			public static com.mobilestore.entity.UserEntity transformUser(com.mobilestore.entity.UserEntity user) {
+				if (user != null) {
+					UserEntity entity = new com.mobilestore.entity.UserEntity();
+					entity.setUserId(user.getUserId());
+					entity.setUsername(user.getUsername());
+					entity.setRole(user.getRole());
+					
+					return entity;
+				}
+				return null;
+			}
+
+			public static UserEntity transformUserEntity(com.mobilestore.entity.UserEntity entity) {
+				if (entity != null) {
+					com.mobilestore.entity.UserEntity newUser = new com.mobilestore.entity.UserEntity();
+					newUser.setUserId(entity.getUserId());
+					newUser.setUsername(entity.getUsername());
+					newUser.setRole(entity.getRole());
+					
+					
+					return newUser;
+				}
+				return null;
+			}
+			public static UserEntity transformCustomer(User customer) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		}
+
